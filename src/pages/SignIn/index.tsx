cdaee60c06fa00962fa562c72a547c4fa393a4e0
Roney Charles <roneycharles@edu.unifor.br>
2020-06-30 18:28:16 -0300

    tooltip

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,39 @@
 import React from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
 
-const SignIn: React.FunctionComponent = () => <h1>SignIn</h1>;
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FunctionComponent = () => (
+  <Container>
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+      <form>
+        <h1>Faça seu login</h1>
+
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Entrar</Button>
+
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+
+      <a href="Login">
+        <FiLogIn />
+        Criar conta
+      </a>
+    </Content>
+    <Background />
+  </Container>
+);
 export default SignIn;
